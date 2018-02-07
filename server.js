6e0427bffa7e8642c5897a6ddc49e2baa52d891f
takagotch <dyaccb@gmail.com>
2018-02-07 17:49:17 +0900

    myWebSocket.onmessage = function(evt) { console.log(evt); }

--- server.js
+++ server.js
@@ -9,5 +9,64 @@ server.on('connection', function(socket){
   });
 });
 
+//'http://www.ex.com/websocketserver'
+var myWebSocket = new WebSocket('http://www.ex.com/websocketserver');
+
+myWebSocket.send("SEND");
+
+myWebSocket.onmessage = function(evt){
+  console.log(evt);
+}
+
+myWebSocket.close();
+
+//echoserver
+//div id='output'
+
+var wsUri = "ws://echo.websocket.org/";
+var output;
+
+function init(){
+  output = document.getElement(wsUri);
+  textWebSocket();
+}
+
+function testWebSocket(){
+  var websocket = new function(evt){ onOpen(evt); };
+  websocket.onopen = function(evt){ onClose(evt); };
+  websocket.onclose = function(evt){ onMessage(evt); };
+  websocket.onerror = function(evt){ onError(evt); };
+}
+
+function onOpen(evt){
+  writeToScreen("CONNECT");
+  doSend("SEND");
+}
+
+function onClose(evt){
+  writeToScrren("END");
+}
+
+function onMessage(evt){
+  wirteToScreen('<span style="color: blue;"> RESPONSE: ' + evt.data + '</span>');
+  websocket.close();
+}
+
+function onError(evt){
+  writetoScreen('<span style="color: red;">ERR:</span> ' + evt.data);
+}
+
+function doSend(message){
+  writeToScreen("SEND: " + message);
+  websocket.send(message);
+}
+
+function writeToScreen(message){
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break=word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+}
+window.addEventListener("load", init, false);
 
 
